ddd24ab1c247498aff729d6f4426c0ab62869c35
David He <david.he@magic.link>
2023-02-20 11:07:06 -0500

    isolating build issue

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,25 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Currency, TransactionHash } from '@tatumio/tatum';
-import { TatumFlowSDK } from '@tatumio/flow';
+// import { Currency, TransactionHash } from '@tatumio/tatum';
+// import { TatumFlowSDK } from '@tatumio/flow';
 
-const flowSDK = TatumFlowSDK({ apiKey: '78177955-ad41-47e7-bb54-1e0c21cdf821', testnet: true });
+// const flowSDK = TatumFlowSDK({ apiKey: '78177955-ad41-47e7-bb54-1e0c21cdf821', testnet: true });
 
 // Test deployment wallets
 let private_key: string, account: string;
 
 const doDeployContract = async () => {
-  const { mnemonic, xpub } = await flowSDK.wallet.generateWallet()
+  /* const { mnemonic, xpub } = await flowSDK.wallet.generateWallet()
   const { address: firstAddress } = await flowSDK.blockchain.generateAddress(xpub, 0)
   account = firstAddress as string
   private_key = await flowSDK.wallet.generatePrivateKeyFromMnemonic(mnemonic, 0)
+  // flowSDK.nft.updateNFTRoyalty
   const txnHash = (await flowSDK.nft.deployNFTSmartContract({
       chain: Currency.FLOW,
       privateKey: private_key,
       account: account,
   })) as TransactionHash;
   const contractAddressRes = await flowSDK.blockchain.smartContractGetAddress(Currency.FLOW, txnHash.txId);
-  return contractAddressRes.contractAddress;
+  return contractAddressRes.contractAddress;*/
+  return '0xabc';
 }
 type Data = {
   contract_address: string | undefined
