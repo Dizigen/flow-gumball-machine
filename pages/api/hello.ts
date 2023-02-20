ed5fa540da00768fdb00cf72fc7af585599a4830
David He <david.he@magic.link>
2023-02-20 11:13:45 -0500

    try tatum api client instead

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-// import { Currency, TransactionHash } from '@tatumio/tatum';
+import { Currency, TransactionHash } from '@tatumio/api-client';
 import { TatumFlowSDK } from '@tatumio/flow';
 
 const flowSDK = TatumFlowSDK({ apiKey: '78177955-ad41-47e7-bb54-1e0c21cdf821', testnet: true });
@@ -9,7 +9,7 @@ const flowSDK = TatumFlowSDK({ apiKey: '78177955-ad41-47e7-bb54-1e0c21cdf821', t
 let private_key: string, account: string;
 
 const doDeployContract = async () => {
-  /* const { mnemonic, xpub } = await flowSDK.wallet.generateWallet()
+  const { mnemonic, xpub } = await flowSDK.wallet.generateWallet()
   const { address: firstAddress } = await flowSDK.blockchain.generateAddress(xpub, 0)
   account = firstAddress as string
   private_key = await flowSDK.wallet.generatePrivateKeyFromMnemonic(mnemonic, 0)
@@ -20,8 +20,7 @@ const doDeployContract = async () => {
       account: account,
   })) as TransactionHash;
   const contractAddressRes = await flowSDK.blockchain.smartContractGetAddress(Currency.FLOW, txnHash.txId);
-  return contractAddressRes.contractAddress;*/
-  return '0xabc';
+  return contractAddressRes.contractAddress;
 }
 type Data = {
   contract_address: string | undefined
