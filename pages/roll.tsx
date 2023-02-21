c3fb2a478122b426f9802c3f706bfb8f938cc020
David He <david.he@magic.link>
2023-02-21 10:04:08 -0500

    fix issue with new accounts not being authorized to pull collection

--- pages/roll.tsx
+++ pages/roll.tsx
@@ -9,16 +9,43 @@ import Image from 'next/image';
 import Link from 'next/link'
 const fcl = require("@onflow/fcl");
 import { TatumFlowSDK } from '@tatumio/flow';
-import { Currency, FlowMintedResult, TransactionHash } from '@tatumio/api-client'
+import { Currency } from '@tatumio/api-client'
 import { NFT_CONTRACT_ADDRESS } from './api/roll';
 
 const inter = Inter({ subsets: ['latin'] });
-const FLOW_TOKEN_OFFSET = 10000000;
 
 fcl.config().put('accessNode.api', 'https://rest-testnet.onflow.org');
 fcl.config().put('challenge.handshake', 'http://access-001.devnet9.nodes.onflow.org:8000');
 
 const flowSDK = TatumFlowSDK({ apiKey: '78177955-ad41-47e7-bb54-1e0c21cdf821', testnet: true });
+
+const getReferenceBlock = async () => {
+  const response = await fcl.send([fcl.getBlock(false)]);
+  const data = await fcl.decode(response);
+  return data.id;
+};
+
+const doAuthorizeAccount = async () => {
+  const AUTHORIZATION_FUNCTION = getMagicInstance().flow.authorization;
+
+  var response = await fcl.send([
+    fcl.transaction`import TatumMultiNFT from 0x87fe4ebd0cddde06
+    transaction {  
+      prepare(signer: AuthAccount) {
+        if signer.borrow<&TatumMultiNFT.Collection>(from: TatumMultiNFT.CollectionStoragePath) == nil {
+          let collection <- TatumMultiNFT.createEmptyCollection()
+          signer.save(<-collection, to: TatumMultiNFT.CollectionStoragePath)
+          signer.link<&TatumMultiNFT.Collection>(TatumMultiNFT.CollectionPublicPath, target: TatumMultiNFT.CollectionStoragePath)
+        }
+      }
+    }`,
+    fcl.ref(await getReferenceBlock()),
+    fcl.proposer(AUTHORIZATION_FUNCTION),
+    fcl.authorizations([AUTHORIZATION_FUNCTION]),
+    fcl.payer(AUTHORIZATION_FUNCTION),
+  ]);
+  return response;
+}
 
 type RollModalProps ={
   showModal: Boolean;
@@ -97,12 +124,12 @@ export default function Login() {
             setIsLoggedIn(loggedIn);
             if (loggedIn) {
                 const { publicAddress: pubAddr } = await getMagicInstance().user.getMetadata();
-                setPublicAddress(pubAddr || '');
+                console.log('pubAddr', pubAddr);
+                setPublicAddress(pubAddr as string);
                 const acc = await fcl.account(pubAddr);
                 setAccountBalance(acc.balance);
-                // const nftAccountBalance = await callToGetBalanceApi(publicAddress);
                 const assets = new Array();
-                const nftAccountBalance = (await flowSDK.nft.getNFTAccountBalance(Currency.FLOW, publicAddress, NFT_CONTRACT_ADDRESS)) as Array<string>;
+                const nftAccountBalance = (await flowSDK.nft.getNFTAccountBalance(Currency.FLOW, pubAddr as string, NFT_CONTRACT_ADDRESS)) as Array<string>;
                 for (let i = 0; i < nftAccountBalance.length; i++) {
                   const assetName = await flowSDK.nft.getNFTMetadataURI(Currency.FLOW, NFT_CONTRACT_ADDRESS, nftAccountBalance[i], publicAddress);
                   assets.unshift((assetName as any).data);
@@ -110,17 +137,21 @@ export default function Login() {
                 setNftBalance(assets);
             }
             setIsLoading(false);
-        }
+          }
      }
-  }, [publicAddress])
+  }, [])
 
   const doLogin = async () => {
+    setIsLoading(true);
     await getMagicInstance().auth.loginWithEmailOTP({ email: userEmail });
     setIsLoggedIn(true);
     const { publicAddress: pubAddr } = await getMagicInstance().user.getMetadata();
     setPublicAddress(pubAddr || '');
     const acc = await fcl.account(pubAddr);
     setAccountBalance(acc.balance);
+    const response = await doAuthorizeAccount();
+    await fcl.tx(response).onceSealed();
+    setIsLoading(false);
   }
 
   const doLogout = async () => {
@@ -129,33 +160,10 @@ export default function Login() {
     setIsLoggedIn(false)
     setIsLoading(false);
   }
-
-  const getReferenceBlock = async () => {
-    const response = await fcl.send([fcl.getBlock(false)]);
-    const data = await fcl.decode(response);
-    return data.id;
-  };
   const doRoll = async () => {
     setShowRollModal(true);
     setStatus('Executing Authorization Function');
-    const AUTHORIZATION_FUNCTION = getMagicInstance().flow.authorization;
-
-    var response = await fcl.send([
-      fcl.transaction`import TatumMultiNFT from 0x87fe4ebd0cddde06
-      transaction {  
-        prepare(signer: AuthAccount) {
-          if signer.borrow<&TatumMultiNFT.Collection>(from: TatumMultiNFT.CollectionStoragePath) == nil {
-            let collection <- TatumMultiNFT.createEmptyCollection()
-            signer.save(<-collection, to: TatumMultiNFT.CollectionStoragePath)
-            signer.link<&TatumMultiNFT.Collection>(TatumMultiNFT.CollectionPublicPath, target: TatumMultiNFT.CollectionStoragePath)
-          }
-        }
-      }`,
-      fcl.ref(await getReferenceBlock()),
-      fcl.proposer(AUTHORIZATION_FUNCTION),
-      fcl.authorizations([AUTHORIZATION_FUNCTION]),
-      fcl.payer(AUTHORIZATION_FUNCTION),
-    ]);
+    const response = await doAuthorizeAccount()
     setStatus('Waiting for function to be sealed')
     await fcl.tx(response).onceSealed();
     setStatus('Rolling for an NFT')
